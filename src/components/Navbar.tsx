2b9a1f526f4736a92c648436352101f952c71826
Hira-Haider-46 <haiderhira46@gmail.com>
2025-02-25 17:46:04 +0500

    responsiveness added in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Menu, X } from "lucide-react";
 
 const Navbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="flex justify-between w-[100%] px-10 py-5 bg-neutral-850 text-white">
-        <h1 className="text-2xl font-600 cursor-pointer hover:font-bold duration-300 ease-out">Password Manager</h1>
-        <ul className="flex gap-15">
-            <li className="text-lg font-500 cursor-pointer hover:font-bold duration-300 ease-out">Home</li>
-            <li className="text-lg font-500 cursor-pointer hover:font-bold duration-300 ease-out">About</li>
-            <li className="text-lg font-500 cursor-pointer hover:font-bold duration-300 ease-out">Contact</li>
+    <nav className="flex items-center justify-between w-full px-6 py-4 text-white">
+      <h1 className="sm:text-xl md:text-2xl font-semibold cursor-pointer hover:font-bold duration-300">
+        Password Manager
+      </h1>
+      <ul className="hidden md:flex space-x-8">
+        <li className="text-lg font-medium cursor-pointer hover:font-bold duration-300">Home</li>
+        <li className="text-lg font-medium cursor-pointer hover:font-bold duration-300">About</li>
+        <li className="text-lg font-medium cursor-pointer hover:font-bold duration-300">Contact</li>
+      </ul>
+      <button
+        className="md:hidden text-white focus:outline-none"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? <X size={28} /> : <Menu size={28} />}
+      </button>
+
+      {isOpen && (
+        <ul className="absolute top-16 left-0 w-full text-white flex flex-col space-y-4 py-5 items-center md:hidden">
+          <li className="text-base font-medium cursor-pointer hover:font-bold duration-300">Home</li>
+          <li className="text-base font-medium cursor-pointer hover:font-bold duration-300">About</li>
+          <li className="text-base font-medium cursor-pointer hover:font-bold duration-300">Contact</li>
         </ul>
+      )}
     </nav>
   );
 };

5477cd9a7677b4d36a1e3559975c7f60a9c8de0b
Hira-Haider-46 <haiderhira46@gmail.com>
2025-02-25 16:28:53 +0500

    navbar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Manager from "./components/Manager";
 
 const App: React.FC = () => {
   return (
     <>
       <Navbar />
+      <Manager />
     </>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="flex justify-between w-[100%] px-10 py-5 bg-teal-500">
+    <nav className="flex justify-between w-[100%] px-10 py-5 bg-neutral-850 text-white">
         <h1 className="text-2xl font-600 cursor-pointer hover:font-bold duration-300 ease-out">Password Manager</h1>
         <ul className="flex gap-15">
             <li className="text-lg font-500 cursor-pointer hover:font-bold duration-300 ease-out">Home</li>

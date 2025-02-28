03d87593fba8044b569ff52ac17dc71ef17d3ba9
Hira-Haider-46 <haiderhira46@gmail.com>
2025-02-28 18:27:45 +0500

    get, post and delete api created

---
+++ backend/server.ts
@@ -0,0 +1,40 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import { MongoClient } from "mongodb";
+import bodyParser from "body-parser";
+
+dotenv.config();
+const app = express();
+const port: number = 3000;
+
+app.use(bodyParser.json());
+
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const dbName = "password_manager";
+
+client.connect();
+
+const db = client.db(dbName);
+
+app.get("/", async (req: Request, res: Response) => {
+  const collection = db.collection("passwords");
+  const findResult = await collection.find({}).toArray();
+  res.send(findResult);
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  const collection = db.collection("passwords");
+  const findResult = await collection.insertOne(req.body);
+  res.send({success: true, result: findResult});
+});
+
+app.delete("/", async (req: Request, res: Response) => {
+  const collection = db.collection("passwords");
+  const findResult = await collection.deleteOne(req.body);
+  res.send({success: true, result: findResult});
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
